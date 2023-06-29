34ee5e43edb563f593025d4a3fb0467ce5830fda
Jorge Velez de Villa <jorgebrianvdv@gmail.com>
2023-06-28 20:48:45 -0500

    cambio de titulo

--- exportaciones.php
+++ exportaciones.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Entradas - Cultivos del Sol</title>
+    <title>Exportaciones - Cultivos del Sol</title>
     <link rel="shortcut icon" href="img/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
